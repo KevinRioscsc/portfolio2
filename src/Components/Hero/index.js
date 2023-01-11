19968934282d7fae3d769a344589a8ec040afb93
Kevin Rios <rioskevin89@gmail.com>
2023-01-10 16:04:33 -0800

    first commit

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -1,46 +1,44 @@
 import React from "react";
 import "./Hero.css";
 import { Link } from "react-router-dom";
-import { Fade } from "react-reveal";
+
 const Hero = () => {
   return (
     <div className="about">
-      <Fade top>
-        <h1 className="primary-header">
-          <span>HEY, I'M</span>
-          <ul className="glow_text">
-            <li>
-              <input type="checkbox" />
-              <div className="letter">K</div>
-            </li>
-            <li>
-              <input type="checkbox" />
-              <div className="letter">E</div>
-            </li>
-            <li>
-              <input type="checkbox" />
-              <div className="letter">V</div>
-            </li>
-            <li>
-              <input type="checkbox" />
-              <div className="letter">I</div>
-            </li>
-            <li>
-              <input type="checkbox" />
-              <div className="letter">N</div>
-            </li>
-          </ul>
-        </h1>
-        <p className="descr">
-          {" "}
-          I'm a full stack developer with the passion to create
-        </p>
-        <p className="descr">Contact me and let's create something special</p>
+      <h1 className="primary-header">
+        <span>HEY, I'M</span>
+        <ul className="glow_text">
+          <li>
+            <input type="checkbox" />
+            <div className="letter">K</div>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <div className="letter">E</div>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <div className="letter">V</div>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <div className="letter">I</div>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <div className="letter">N</div>
+          </li>
+        </ul>
+      </h1>
+      <p className="descr">
+        {" "}
+        I'm a full stack developer with the passion to create
+      </p>
+      <p className="descr">Contact me and let's create something special</p>
 
-        <Link to={"/projects"}>
-          <button>VIEW PROJECTS</button>
-        </Link>
-      </Fade>
+      <Link to={"/projects"}>
+        <button>VIEW PROJECTS</button>
+      </Link>
     </div>
   );
 };
--- src/Components/Pages/contact.js
+++ src/Components/Pages/contact.js
@@ -1,17 +1,16 @@
-import React from 'react'
-import ContactMe from '../Contact'
-import { Fade } from 'react-reveal'
+import React from "react";
+import ContactMe from "../Contact";
 
 const Contact = () => {
   return (
-    <div className='main'> 
-        <Fade>
-            <h2 className='second-header small'>Coffee?</h2>
-            <sub className='secondary-sub small2'>Ask me anything! Ill get back to you as soon as possible.</sub>
-        <ContactMe/>
-        </Fade>
+    <div className="main">
+      <h2 className="second-header small">Coffee?</h2>
+      <sub className="secondary-sub small2">
+        Ask me anything! Ill get back to you as soon as possible.
+      </sub>
+      <ContactMe />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
