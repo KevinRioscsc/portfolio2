8b8ab8bc2824a96c2291e0cec5c1216cf39371ec
Kevin Rios <rioskevin89@gmail.com>
2023-02-21 16:16:39 -0800

    changes

--- src/Components/Pages/projects.js
+++ src/Components/Pages/projects.js
@@ -5,17 +5,19 @@ import { projects } from "../../ProjectsData";
 const Projects = () => {
   return (
     <>
-      <div className="main proMain">
-        <h2 className="second-header small ">Portfolio</h2>
-        <sub className="secondary-sub small2">
-          These are my favorite projects I've worked on for the past year. Have
-          a look around.
-        </sub>
-        <div className="space">
-          <div className="flex">
-            {projects.map((item, index) => {
-              return <ProjectBox key={index} {...item} />;
-            })}
+      <div className="mainApp">
+        <div className="main proMain">
+          <h2 className="second-header small ">Portfolio</h2>
+          <sub className="secondary-sub small2">
+            These are my favorite projects I've worked on for the past year.
+            Have a look around.
+          </sub>
+          <div className="space">
+            <div className="flex">
+              {projects.map((item, index) => {
+                return <ProjectBox key={index} {...item} />;
+              })}
+            </div>
           </div>
         </div>
       </div>
